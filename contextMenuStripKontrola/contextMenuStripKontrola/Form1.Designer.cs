f9c3a4d1cc90f3236bfd0b53f32c7d6b330d5ffe
MatejLukasMarunica <145869520+MatejLukasMarunica@users.noreply.github.com>
2023-10-11 09:29:16 +0200

    Add files via upload

---
+++ contextMenuStripKontrola/contextMenuStripKontrola/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace contextMenuStripKontrola
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.lijevoNaDesnoCTRLDToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.desnoNaLijevoCTRLLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.textBox1.Location = new System.Drawing.Point(69, 115);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(129, 28);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.ContextMenuStrip = this.contextMenuStrip1;
+            this.textBox2.Location = new System.Drawing.Point(281, 115);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(129, 28);
+            this.textBox2.TabIndex = 1;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lijevoNaDesnoCTRLDToolStripMenuItem,
+            this.desnoNaLijevoCTRLLToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(209, 70);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // lijevoNaDesnoCTRLDToolStripMenuItem
+            // 
+            this.lijevoNaDesnoCTRLDToolStripMenuItem.Name = "lijevoNaDesnoCTRLDToolStripMenuItem";
+            this.lijevoNaDesnoCTRLDToolStripMenuItem.Size = new System.Drawing.Size(208, 22);
+            this.lijevoNaDesnoCTRLDToolStripMenuItem.Text = "Lijevo na desno CTRL + D";
+            // 
+            // desnoNaLijevoCTRLLToolStripMenuItem
+            // 
+            this.desnoNaLijevoCTRLLToolStripMenuItem.Name = "desnoNaLijevoCTRLLToolStripMenuItem";
+            this.desnoNaLijevoCTRLLToolStripMenuItem.Size = new System.Drawing.Size(208, 22);
+            this.desnoNaLijevoCTRLLToolStripMenuItem.Text = "Desno na lijevo CTRL + L";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Upotreba ContextMenuStrip Kontrole";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem lijevoNaDesnoCTRLDToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem desnoNaLijevoCTRLLToolStripMenuItem;
+    }
+}
+
